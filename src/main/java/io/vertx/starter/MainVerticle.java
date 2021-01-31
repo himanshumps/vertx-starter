0ceedd8f5cdcc6f8e6c057f81e192f0435011a2a
Himanshu Gupta <himanshu.mps@gmail.com>
2021-02-01 01:55:41 +0530

    Update MainVerticle.java

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -10,7 +10,7 @@ public class MainVerticle extends AbstractVerticle {
 	if(System.getenv("PORT") != null) 
 		port=Integer.parseInt(System.getenv("PORT"));  
 	vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Hello Vert.x !"))
+        .requestHandler(req -> req.response().end("Hello Vert.x - kubectl test !"))
         .listen(port);
   }
 
