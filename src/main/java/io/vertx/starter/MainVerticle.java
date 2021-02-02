b0a28d21052d127432a6d4db03b2cf2bcfb1e980
Himanshu Gupta <himanshu.mps@gmail.com>
2021-02-02 16:14:01 +0530

    Update MainVerticle.java

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -10,7 +10,7 @@ public class MainVerticle extends AbstractVerticle {
 	if(System.getenv("PORT") != null) 
 		port=Integer.parseInt(System.getenv("PORT"));  
 	vertx.createHttpServer()
-        .requestHandler(req -> req.response().end("Welcome to Learning Axis Hackathon - Team AiKiDo - Demo !!"))
+        .requestHandler(req -> req.response().end("Welcome to Learning Axis Hackathon - Team AiKiDo - Demo - Final !!"))
         .listen(port);
   }
 
